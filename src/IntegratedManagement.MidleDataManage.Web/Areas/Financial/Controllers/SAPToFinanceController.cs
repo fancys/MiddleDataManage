49ee61a0d4536b86c0a91ec405a6e2c97f9a8b9a
Administrator <Administrator@SAPINTERFACE>
2017-11-06 20:07:51 +0800

    add view of sync data to finance system

---
+++ src/IntegratedManagement.MidleDataManage.Web/Areas/Financial/Controllers/SAPToFinanceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntegratedManagement.MidleDataManage.Web.Areas.Financial.Controllers
+{
+    // SAP系统主数据同步到财务帐套
+    public class SAPToFinanceController : Controller
+    {
+        // GET: Financial/SAPToFinance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
